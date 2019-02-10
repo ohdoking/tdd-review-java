750a67cc237ebe4239ff433417b498fa47457253
Kenny Oh <ohdoking@gmail.com>
2019-02-10 19:34:28 +0100

    [Refactoring] modify TicTacToe class more readable code

--- src/main/java/com/ohdoking/tddjava/ch03tictactoe/TicTacToe.java
+++ src/main/java/com/ohdoking/tddjava/ch03tictactoe/TicTacToe.java
@@ -28,7 +28,7 @@ public class TicTacToe {
         checkAxis(y);
         lastPlayer = nextPlayer();
         setBox(x, y, lastPlayer);
-        if (isWin()) {
+        if (isWin(x,y)) {
             return lastPlayer + " is the winner";
         }
         else if (isDraw()) {
@@ -60,22 +60,20 @@ public class TicTacToe {
         return 'X';
     }
 
-    private boolean isWin() {
-        int playerTotal = lastPlayer * SIZE;
-        char diagonal1 = '\0';
-        char diagonal2 = '\0';
+    private boolean isWin(int x, int y) {
+        int playerTotal = lastPlayer * 3;
+        char horizontal, vertical, diagonal1, diagonal2;
+        horizontal = vertical = diagonal1 = diagonal2 = '\0';
         for (int i = 0; i < SIZE; i++) {
+            horizontal += board[i][y - 1];
+            vertical += board[x - 1][i];
             diagonal1 += board[i][i];
             diagonal2 += board[i][SIZE - i - 1];
-            if (board[0][i] + board[1][i] + board[2][i] == playerTotal) {
-                return true;
-            }
-            else if (board[i][0] + board[i][1] + board[i][2] == playerTotal){
-                return true;
-            }
-
         }
-        if (diagonal1 == playerTotal || diagonal2 == playerTotal) {
+        if (horizontal == playerTotal
+                || vertical == playerTotal
+                || diagonal1 == playerTotal
+                || diagonal2 == playerTotal) {
             return true;
         }
         return false;
