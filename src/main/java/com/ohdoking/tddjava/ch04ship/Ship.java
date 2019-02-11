4075671c5a8c15271bab807958446ce170507a36
Kenny Oh <ohdoking@gmail.com>
2019-02-11 11:18:36 +0100

    Implement test and code as follow when instantiated then location is set

--- src/main/java/com/ohdoking/tddjava/ch04ship/Ship.java
+++ src/main/java/com/ohdoking/tddjava/ch04ship/Ship.java
@@ -1,4 +1,14 @@
 package com.ohdoking.tddjava.ch04ship;
 
 public class Ship {
+
+    private Location location;
+
+    public Ship(Location location) {
+        this.location = location;
+    }
+
+    public Location getLocation() {
+        return location;
+    }
 }
--- src/test/java/com/ohdoking/tddjava/ch04ship/ShipSpec.java
+++ src/test/java/com/ohdoking/tddjava/ch04ship/ShipSpec.java
@@ -1,7 +1,25 @@
 package com.ohdoking.tddjava.ch04ship;
 
+import org.junit.Test;
+
 import static org.junit.Assert.*;
 
+/**
+ * ShipSpec
+ *
+ * @author ohdoking
+ *
+ * You are given the initial starting point (x, y) of a ship and the direction (N, S, E, or W) it is facing.
+ *
+ */
 public class ShipSpec {
+
+    @Test
+    public void whenInstantiatedThenLocationIsSet() {
+        Location location = new Location(
+                new Point(21, 13), Direction.NORTH);
+        Ship ship = new Ship(location);
+        assertEquals(ship.getLocation(), location);
+    }
 
 }
