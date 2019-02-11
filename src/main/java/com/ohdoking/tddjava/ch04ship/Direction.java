6eace121692d0bc1eb9a8b22b6a68b1a572ce40a
Kenny Oh <ohdoking@gmail.com>
2019-02-11 12:07:49 +0100

    change valuable name.

--- src/main/java/com/ohdoking/tddjava/ch04ship/Direction.java
+++ src/main/java/com/ohdoking/tddjava/ch04ship/Direction.java
@@ -7,16 +7,16 @@ public enum Direction {
     WEST(3, 'W'),
     NONE(4, 'X');
 
-    private int i;
+    private int index;
     private char cardinalPoint;
 
-    Direction(int i, char cardinalPoint) {
-        this.i = i;
+    Direction(int index, char cardinalPoint) {
+        this.index = index;
         this.cardinalPoint = cardinalPoint;
     }
 
-    public int getI() {
-        return i;
+    public int getIndex() {
+        return index;
     }
 
     public char getCardinalPoint() {
