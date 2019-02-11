abee4602929697015409b205d31e2feee7df683d
Kenny Oh <ohdoking@gmail.com>
2019-02-11 10:43:04 +0100

    create class

---
+++ src/main/java/com/ohdoking/tddjava/ch04ship/Point.java
@@ -0,0 +1,4 @@
+package com.ohdoking.tddjava.ch04ship;
+
+public class Point {
+}
---
+++ src/test/java/com/ohdoking/tddjava/ch04ship/LocationSpec.java
@@ -0,0 +1,7 @@
+package com.ohdoking.tddjava.ch04ship;
+
+import static org.junit.Assert.*;
+
+public class LocationTest {
+
+}
