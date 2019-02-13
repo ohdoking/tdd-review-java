46c6810fb1d721969bf04cd15211a47af977d3ef
Kenny Oh <ohdoking@gmail.com>
2019-02-13 19:36:39 +0100

    how to add task in Gradle to integrate test

---
+++ src/test/java/com/ohdoking/tddjava/ch06tictactoe/TicTacToeInteg.java
@@ -0,0 +1,16 @@
+package com.ohdoking.tddjava.ch06tictactoe;
+
+import com.ohdoking.tddjava.ch03tictactoe.TicTacToe;
+import org.junit.Test;
+
+import java.net.UnknownHostException;
+
+import static org.junit.Assert.assertEquals;
+
+public class TicTacToeInteg {
+       @Test
+       public void givenMongoDbIsRunningWhenPlayThenNoException() throws UnknownHostException {
+           TicTacToe ticTacToe = new TicTacToe();
+           assertEquals("No winner", ticTacToe.play(1, 1));
+       }
+}
