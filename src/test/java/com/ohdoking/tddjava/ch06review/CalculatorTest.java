ca3673e57b5cdae3ce72e6932d9360536a71e84a
Kenny Oh <ohdoking@gmail.com>
2019-02-14 17:35:22 +0100

    separate Test class by each function

--- src/test/java/com/ohdoking/tddjava/ch06review/CalculatorTest.java
+++ src/test/java/com/ohdoking/tddjava/ch06review/CalculatorTest.java
@@ -5,9 +5,6 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
 
-import java.util.List;
-
-import static org.junit.Assert.*;
 import static org.mockito.Mockito.*;
 
 public class CalculatorTest {
@@ -15,61 +12,13 @@ public class CalculatorTest {
     Calculator calculator;
     CustomDB db;
 
-    List list;
-
     @Rule
     public ExpectedException exception = ExpectedException.none();
-
 
     @Before
     public void before(){
         db = mock(CustomDB.class);
         calculator = new Calculator(db);
-        list = mock(List.class);
-    }
-
-    @Test
-    public void whenAddValueAndThenClear(){
-        // use mock
-        list.add("one");
-        list.add("two");
-        list.clear();
-
-        // verification
-        verify(list).add("one");
-        verify(list).add("two");
-        verify(list).clear();
-    }
-
-    @Test
-    public void useStubbing(){
-        // stubbing
-        when(list.get(0)).thenReturn("first");
-        when(list.get(1)).thenThrow(new RuntimeException());
-        doReturn("second").when(list).get(2);
-
-        System.out.println(list.get(2)); // second
-        System.out.println(list.get(0)); // first
-        System.out.println(list.get(1)); // Runtime exception 발생
-        System.out.println(list.get(10)); // null
-
-        verify(list).get(0);
-
-    }
-
-    @Test
-    public void useArguementMatcher(){
-        when(list.get(anyInt())).thenReturn("int");
-        when(list.add(anyFloat())).thenReturn(true);
-        when(list.add(anyString())).thenReturn(true);
-
-        System.out.println(list.get(999)); // int
-        System.out.println(list.add(3.3)); // true
-        System.out.println(list.add("string")); // true
-
-        verify(list).get(anyInt());
-        verify(list).add(anyFloat());
-        verify(list).add(eq("string"));
     }
 
     @Test
@@ -87,6 +36,4 @@ public class CalculatorTest {
         exception.expect(RuntimeException.class);
         calculator.add(1,2);
     }
-
-
 }
---
+++ src/test/java/com/ohdoking/tddjava/ch06review/MockingSpec.java
@@ -0,0 +1,143 @@
+package com.ohdoking.tddjava.ch06review;
+
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+import org.mockito.InOrder;
+
+import java.util.List;
+
+import static org.mockito.Matchers.anyFloat;
+import static org.mockito.Matchers.anyInt;
+import static org.mockito.Matchers.anyString;
+import static org.mockito.Matchers.eq;
+import static org.mockito.Mockito.*;
+
+public class MockingSpec {
+
+    List list;
+
+    @Rule
+    public ExpectedException exception = ExpectedException.none();
+
+
+    @Before
+    public void before(){
+        list = mock(List.class);
+    }
+
+    @Test
+    public void whenAddValueAndThenClear(){
+        // use mock
+        list.add("one");
+        list.add("two");
+        list.clear();
+
+        // verification
+        verify(list).add("one");
+        verify(list).add("two");
+        verify(list).clear();
+    }
+
+    @Test
+    public void useStubbing(){
+        // stubbing
+        when(list.get(0)).thenReturn("first");
+        when(list.get(1)).thenThrow(new RuntimeException());
+        doReturn("second").when(list).get(2);
+
+        System.out.println(list.get(2)); // second
+        System.out.println(list.get(0)); // first
+        System.out.println(list.get(1)); // Runtime exception 발생
+        System.out.println(list.get(10)); // null
+
+        verify(list).get(0);
+
+    }
+
+    @Test
+    public void useArguementMatcher(){
+        when(list.get(anyInt())).thenReturn("int");
+        when(list.add(anyFloat())).thenReturn(true);
+        when(list.add(anyString())).thenReturn(true);
+
+        System.out.println(list.get(999)); // int
+        System.out.println(list.add(3.3)); // true
+        System.out.println(list.add("string")); // true
+
+        verify(list).get(anyInt());
+        verify(list).add(anyFloat());
+        verify(list).add(eq("string"));
+    }
+
+    @Test
+    public void verifyingExactNumberOfInvocations(){
+        list.add("once");
+
+        list.add("twice");
+        list.add("twice");
+
+        list.add("three times");
+        list.add("three times");
+        list.add("three times");
+
+        verify(list).add("once"); // times(1) 기본값
+        verify(list, times(1)).add("once");
+
+        verify(list, times(2)).add("twice");
+        verify(list, times(3)).add("three times");
+
+        verify(list, never()).add("never happened"); // 호출된 적 없음
+
+        //verify(list, atLeastOnce()).add("three times"); // 최소 한 번
+        //verify(list, atLeast(2)).add("five times"); // 최소 두 번
+        //verify(list, atMost(5)).add("three times"); // 최대 다섯 번
+    }
+
+    @Test
+    public void stubbingVoidMethodsWithExceptions(){
+        doThrow(new RuntimeException()).when(list).clear();
+
+        exception.expect(RuntimeException.class);
+        list.clear(); // RuntimeException
+    }
+
+    @Test
+    public void verificationInOrder() {
+        // single mock
+        List singleMock = mock(List.class);
+
+        singleMock.add("first");
+        singleMock.add("second");
+
+        InOrder inOrder = inOrder(singleMock);
+
+        inOrder.verify(singleMock).add("first");
+        inOrder.verify(singleMock).add("second");
+
+        // multiple mocks
+        List firstMock = mock(List.class);
+        List secondMock = mock(List.class);
+
+        //using mocks
+        firstMock.add("first");
+        secondMock.add("second");
+
+        inOrder = inOrder(firstMock, secondMock); // pass multiple mocks to verify
+
+        inOrder.verify(firstMock).add("first");
+        inOrder.verify(secondMock).add("second");
+    }
+
+    @Test
+    public void findingRedundantInvocations(){
+        list.add("one");
+        list.add("two");
+
+        verify(list).add("one");
+        //verify(list).add("two");// if remove fail
+
+        verifyNoMoreInteractions(list); // fail here
+    }
+}
